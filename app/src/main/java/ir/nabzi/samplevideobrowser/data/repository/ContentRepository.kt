e40e6dd0bc71ae60c11bdbd109956408b56e8a45
z.modir <zeinab.modir@gmail.com>
2021-06-03 23:43:00 +0430

    Fixed ellipsize for content item text

--- app/src/main/java/ir/nabzi/samplevideobrowser/data/repository/ContentRepository.kt
+++ app/src/main/java/ir/nabzi/samplevideobrowser/data/repository/ContentRepository.kt
@@ -89,17 +89,17 @@ class ContentRemoteDataSource(val apiServices: ApiService) {
 
     private fun getMockData(search: String = ""): List<Content> {
         return arrayListOf(
-                Content("id1" , "avideo 1" , "Video 1 Video 1 Video 1 Video 1" ,
+                Content("id1" , "Red color", "Red is the color at the long wavelength end of the visible spectrum of light, next to orange and opposite violet. It has a dominant wavelength of approximately 625–740 nanometres.[1] It is a primary color in the RGB color model and the CMYK color model" ,
                 "https://www.healthcareitnews.com/sites/hitn/files/120319%20CaroMont%20Regional%20Medical%20Center%20712.jpg",
                 "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_5mb.mp4",
                         70 , 2,0,3
                         ),
-                Content("id2" , "avadeo 2" , "Video 2 Video 1 Video 1 Video 1" ,
+                Content("id2" , "Blue" , "Blue is one of the three primary colours of pigments in painting and traditional colour theory, as well as in the RGB colour model. It lies between violet and green on the spectrum of visible light. The eye perceives blue when observing light with a dominant wavelength between approximately 450 and 495 nanometres." ,
                         "https://cdn.britannica.com/s:690x388,c:crop/12/130512-004-AD0A7CA4/campus-Riverside-Ottawa-The-Hospital-Ont.jpg",
                         "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_5mb.mp4",
                         20 , 5,4,2
                 ),
-                Content("id3" , "bvideo 3" , "Video3  Video 1 Video 1 Video 1" ,
+                Content("id3" , "Black White Black White Black White Black White" , "Black is a color which results from the absence or complete absorption of visible light. It is an achromatic color, without hue, like white and gray." ,
                         "http://riyainfosystems.com/wp-content/uploads/2019/06/5b76b45cbb3a6.jpg",
                         "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_5mb.mp4",
                         21 , 5,1,10
